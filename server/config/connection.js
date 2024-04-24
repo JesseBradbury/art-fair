bc54d5ad66469d465b5b722e9d7ffe89fb840f7e
JB <bradburyjesse7@gmail.com>
2024-04-23 22:16:37 -0700

    Adding for Render and Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// I've added the .env with my new AtlasDB user and pass.
 mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/art-fair");
 
 module.exports = mongoose.connection;
